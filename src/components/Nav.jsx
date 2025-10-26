7522d50cc6950669fe7aea0ecb7fed370369af5a
Danny Mettler <danieljamespm@gmail.com>
2025-10-26 15:22:22 -0400

    working on nav bar.

---
+++ src/components/Nav.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <nav className='h-35 text-white border-b-amber-400 border-b-2'>
+            <ul className='flex justify-center px-10 bg-black h-full items-center font-slabo font-bold text-gold  '>
+                <li>Home</li>
+                <li>Menu</li>
+                <li>History</li>
+                <li>Entertainment</li>
+                <li>Contact Us</li>
+                <li>Beer List</li>
+                <li>Employment Application</li>
+            </ul>
+
+        </nav>
+    )
+}
+
+export default Nav
