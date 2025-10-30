91c403f4de23489a1dc4e7c27a5c2327b72d8204
Danny Mettler <danieljamespm@gmail.com>
2025-10-30 00:38:54 -0400

    changed wording on some of the nav links.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -23,11 +23,11 @@ const Nav = () => {
                 <ul className="hidden lg:flex space-x-8 font-slabo font-bold text-gold text-xl">
                     <li className="hover:text-white transition-colors duration-500 ease-in-out"><a href="#">Home</a></li>
                     <li className="hover:text-white transition-colors duration-500 ease-in-out"><a href="#">Menu</a></li>
-                    <li className="hover:text-white transition-colors duration-500 ease-in-out"><a href="#">History</a></li>
+                    <li className="hover:text-white transition-colors duration-500 ease-in-out"><a href="#">Our Story</a></li>
                     <li className="hover:text-white transition-colors duration-500 ease-in-out"><a href="#">Entertainment</a></li>
                     <li className="hover:text-white transition-colors duration-500 ease-in-out"><a href="#">Contact Us</a></li>
                     <li className="hover:text-white transition-colors duration-500 ease-in-out"><a href="#">Beer List</a></li>
-                    <li className="hover:text-white transition-colors duration-500 ease-in-out"><a href="#">Employment Application</a></li>
+                    <li className="hover:text-white transition-colors duration-500 ease-in-out"><a href="#">Apply Now</a></li>
                 </ul>
 
                 {/* Mobile Hamburger */}
@@ -41,11 +41,11 @@ const Nav = () => {
                 <ul className="lg:hidden bg-black text-gold flex flex-col items-center space-y-4 py-4 font-slabo font-bold">
                     <li className="hover:text-white transition-colors duration-500 ease-in-out"><a href="#">Home</a></li>
                     <li className="hover:text-white transition-colors duration-500 ease-in-out"><a href="#">Menu</a></li>
-                    <li className="hover:text-white transition-colors duration-500 ease-in-out"><a href="#">History</a></li>
+                    <li className="hover:text-white transition-colors duration-500 ease-in-out"><a href="#">Our Story</a></li>
                     <li className="hover:text-white transition-colors duration-500 ease-in-out"><a href="#">Entertainment</a></li>
                     <li className="hover:text-white transition-colors duration-500 ease-in-out"><a href="#">Contact Us</a></li>
                     <li className="hover:text-white transition-colors duration-500 ease-in-out"><a href="#">Beer List</a></li>
-                    <li className="hover:text-white transition-colors duration-500 ease-in-out"><a href="#">Employment Application</a></li>
+                    <li className="hover:text-white transition-colors duration-500 ease-in-out"><a href="#">Apply Now</a></li>
                 </ul>
             )}
         </nav>
